38a67f29e3f6972df1a7fea3df79e16afe2bf8c5
Keith Posehn <keith.posehn@gmail.com>
2015-01-31 09:27:10 -0800

    fixed env var names in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  http_basic_authenticate_with name: ENV["username"], password: ENV["password"] if Rails.env.production?
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"] if Rails.env.production?
   
 end
